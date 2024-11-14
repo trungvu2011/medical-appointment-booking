c35a1a2e6b5531919310a92d0a2360f9c6c9ddc2
trungvu2011 <trungvd2011@gmail.com>
2024-11-14 23:35:23 +0700

    MVC model

--- backend/src/route/web.js
+++ backend/src/route/web.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import homeController from '../controllers/homeController';
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello World');
-    });
+    router.get('/', homeController.getHomePage);
 
     router.get('/about', (req, res) => {
         return res.send('About Page');
