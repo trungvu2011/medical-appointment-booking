20fb2db93bbfce88871fde89763359c2278ca560
trungvu2011 <trungvd2011@gmail.com>
2024-11-19 18:17:33 +0700

    Edit homepage

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './HeroSection.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserDoctor, faFileLines } from '@fortawesome/free-solid-svg-icons';
+
 function HeroSection() {
     return (
         <section className="hero-section">
@@ -9,12 +12,18 @@ function HeroSection() {
                 <p>Giờ đây bạn có thể đặt lịch trước khi tới khám và xem được kết quả khám online nhanh chóng mọi lúc mọi nơi</p>
                 <div className="services">
                     <div className="service-card">
-                        <h3>Đặt lịch khám</h3>
-                        <p>Đặt lịch khám online trước khi tới viện</p>
+                        <FontAwesomeIcon className='icon' icon={faUserDoctor} size='3x' />
+                        <div className='card-content'>
+                            <h3>Đặt lịch khám</h3>
+                            <p>Đặt lịch khám online trước khi tới viện</p>
+                        </div>
                     </div>
                     <div className="service-card">
-                        <h3>Hồ sơ sức khỏe</h3>
-                        <p>Xem và lưu trữ kết quả khám bệnh</p>
+                        <FontAwesomeIcon className='icon' icon={faFileLines} size='3x' />
+                        <div className='card-content'>
+                            <h3>Hồ sơ sức khỏe</h3>
+                            <p>Xem và lưu trữ kết quả khám bệnh</p>
+                        </div>
                     </div>
                 </div>
             </div>
