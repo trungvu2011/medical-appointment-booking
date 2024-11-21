e9ab3da95a2cd4dd8785bbe08a0d62e8b05b36b0
sudior04 <nhchien2004@gmail.com>
2024-11-22 01:42:24 +0700

    recommit database ver3.2

--- backend/src/migrations/20241121171928-create-schedule-table.js
+++ backend/src/migrations/20241121171928-create-schedule-table.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Schedule_tables', {
-      id: {
+      schedule_table_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
