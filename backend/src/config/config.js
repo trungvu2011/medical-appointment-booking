79d043dcbf33017a28c31782995b3b82adf31ca1
trungvu2011 <trungvd2011@gmail.com>
2024-11-24 00:28:40 +0700

    reconfig backend

---
+++ backend/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '../.env' });
+
+console.log(process.env.DB_PASSWORD)
+module.exports = {
+    development: {
+        username: 'root',
+        password: process.env.DB_PASSWORD || null,
+        database: process.env.DB_NAME || 'database_development',
+        host: process.env.DB_HOST || '127.0.0.1',
+        dialect: 'mysql'
+    },
+    test: {
+        username: process.env.DB_TEST_USERNAME || 'root',
+        password: process.env.DB_TEST_PASSWORD || null,
+        database: process.env.DB_TEST_NAME || 'database_test',
+        host: process.env.DB_TEST_HOST || '127.0.0.1',
+        dialect: 'mysql'
+    },
+    production: {
+        username: process.env.DB_PROD_USERNAME || 'root',
+        password: process.env.DB_PROD_PASSWORD || null,
+        database: process.env.DB_PROD_NAME || 'database_production',
+        host: process.env.DB_PROD_HOST || '127.0.0.1',
+        dialect: 'mysql'
+    }
+};
