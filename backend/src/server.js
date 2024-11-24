11aed25e00ab9e79bedfa9361d3d6d4b6a9e35f7
trungvu2011 <trungvd2011@gmail.com>
2024-11-24 12:47:54 +0700

    Fix cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,18 @@ import cors from 'cors';
 
 let app = express();
 
+const allowedOrigins = ['http://localhost:3000', 'http://192.168.1.209:3000'];
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: (origin, callback) => {
+        // Nếu không có origin (vd: Postman), hoặc nằm trong danh sách allowedOrigins, cho phép
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các method được phép
+    credentials: true, // Cho phép gửi cookie nếu cần
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
