9a1f8e686f3970fcfdedf50dbfe69ff4a08bf685
trungvu2011 <trungvd2011@gmail.com>
2024-11-29 21:31:47 +0700

    Fix api relative url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage';
 import RecordPage from './pages/RecordPage';
+import axios from 'axios';
 
 function App() {
+  axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
   return (
     <Router>
       <Routes>
