ce8e88dbb9b666db8af19582ff8a273bd88fc985
trungvu2011 <trungvd2011@gmail.com>
2024-12-29 23:45:15 +0700

    config production with docker

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,25 +2,27 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env'
 
 module.exports = {
     development: {
-        username: 'root',
-        password: process.env.DB_PASSWORD || null,
-        database: process.env.DB_NAME || 'database_development',
-        host: process.env.DB_HOST || '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        timezone: '+07:00',
         logging: false
     },
     test: {
-        username: process.env.DB_TEST_USERNAME || 'root',
-        password: process.env.DB_TEST_PASSWORD || null,
-        database: process.env.DB_TEST_NAME || 'database_test',
-        host: process.env.DB_TEST_HOST || '127.0.0.1',
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
         dialect: 'mysql'
     },
     production: {
-        username: process.env.DB_PROD_USERNAME || 'root',
-        password: process.env.DB_PROD_PASSWORD || null,
-        database: process.env.DB_PROD_NAME || 'database_production',
-        host: process.env.DB_PROD_HOST || '127.0.0.1',
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
         dialect: 'mysql'
     }
 };
